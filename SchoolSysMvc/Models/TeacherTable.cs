9b9f9a85df1af078a8d9bb78d5b9453fe7215a83
Zahid Gul <zgull429@gmail.com>
2025-11-11 23:53:30 +0500

    update MVC Dependencey

---
+++ SchoolSysMvc/Models/TeacherTable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SchoolSysMvc.Models;
+
+public partial class TeacherTable
+{
+    public int TeacherId { get; set; }
+
+    public string? TeacherName { get; set; }
+
+    public string? TeacherEmail { get; set; }
+
+    public string? TeacherPhone { get; set; }
+
+    public DateTime? TeacherHireDate { get; set; }
+
+    public string? TeacherQualification { get; set; }
+
+    public bool? TeacherIsActive { get; set; }
+}
